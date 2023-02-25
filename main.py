717c6933662687f39e78411dffa69c5499b2feda
MedddAmine <81730175+MedddAmine@users.noreply.github.com>
2023-02-25 23:55:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,19 @@ x=110
 
 def DetectBaseAudio():
     global i
-    pyautogui.moveTo(xBORDER, yBORDER)
+    #if it detects any color other than white in the vertical line it means that it reached the base line and modify the vertical variable
     while im1.getpixel((xBORDER, i)) == color:
         i = i + 1
-    print(im1.getpixel((i, yBORDER)))
     i=i+5
 
 def SelectParts():
     global x
+    #if it detects any color other than white in the horizontal line it means that it reached the voice it press down in the left button in the mouse
     while im1.getpixel((x, i)) == color:
         x = x + 1
     pyautogui.moveTo(x, i)
     pyautogui.mouseDown(button='left')
+    #if it detects the color white in the horizontal line it means that it reached the end of the voice it press up in the left button in the mouse which ends the selection
     while im1.getpixel((x, i)) != color or im1.getpixel((x+5, i)) != color or im1.getpixel((x+3, i)) != color:
         x = x + 5
     pyautogui.moveTo(x, i)
@@ -30,8 +31,11 @@ def SelectParts():
 def Save():
     for save in range(0,12):
         SelectParts()
+        #Pressing File button coordinates (x,y)
         pyautogui.click(5, 35)
+        #Pressing Save as wav button coordinates (x,y)
         pyautogui.click(5,120)
+        #File extracted name
         number = str(save+1)
         pyautogui.write(number+'.wav')
         pyautogui.press('enter')
